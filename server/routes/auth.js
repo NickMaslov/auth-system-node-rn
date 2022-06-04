11fb0f1954054618eb472d62f5da31c1212d3a30
Nick Maslov <kategorychnost@gmail.com>
2022-06-04 01:24:45 -0400

    created registraion with validation , hashing password, checking existing user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,47 @@
 const express = require('express');
+const { validationResult, check } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
 const User = require('../models/User');
 
-router.post('/register', async (req, res) => {
+const validate = [
+  check('fullName')
+    .isLength({ min: 2 })
+    .withMessage('Your full name is required'),
+  check('email').isEmail().withMessage('Please provide a valid email'),
+  check('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters'),
+];
+
+router.post('/register', validate, async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const userExist = await User.findOne({ email: req.body.email });
+  if (userExist) return res.status(400).send('Email already exist');
+
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
   try {
     const user = new User({
       fullName: req.body.fullName,
       email: req.body.email,
-      password: req.body.password,
+      password: hashPassword,
     });
 
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({
+      id: savedUser._id,
+      fullName: savedUser.fullName,
+      email: savedUser.email,
+    });
   } catch (error) {
     res.status(400).send(error);
   }
